9c3bea0019b6d6d4157804186526b281f4a30fee
Vittorio Gatto <gattovitto21@.com>
2024-01-07 22:15:50 +0100

    import fa sul progetto e ritocco navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,10 +1,13 @@
 
-<a class="btn btn-primary"  data-bs-toggle="offcanvas" href="#offcanvasDark" role="button" aria-controls="offcanvasExample">
-  Navbar
-</a>
+ <a class="btn m-4 btn-cus-nav"  data-bs-toggle="offcanvas" href="#offcanvasDark" role="button" aria-controls="offcanvasExample">
+  <i class="fa-solid fa-bars fs-5"></i>
+</a> 
+
+
 
 
 <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDark" aria-labelledby="offcanvasDarkLabel">
+  <div class="d-flex justify-content-end m-3"><button type="button" class="btn-close btn-close-white  " data-bs-dismiss="offcanvas" aria-label="Close"></button></div>
   @auth
   <div class="d-flex justify-content-center"><img class="UserImg my-4" src="https://picsum.photos/200" alt=""></div>
   @endauth
@@ -13,35 +16,35 @@
     <h5 class="offcanvas-title" id="offcanvasDarkLabel">{{Auth::user()->name}}</h5>
     @endauth
     {{-- QUI INSERIREMO IMG DELL'UTENTE INTATO NOME--}}
-    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    
     
   </div>
   <div class="offcanvas-body">
-    <p><nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <p><nav class="navbar navbar-expand-lg bg-body-trasparent">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">E-presto</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <a class="navbar-brand text-white" href="#">E-presto</a>
+        <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <spa><i class="fa-solid fa-ellipsis fs-5 text-white"></i></spa>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="{{route('homepage')}}">Home</a>
+              <a class="nav-link active text-white" aria-current="page" href="{{route('homepage')}}"><i class="fa-solid fa-house fs-5"></i></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
+              <a class="nav-link text-white" href="#">Link</a>
             </li>
             @auth
-            <li class="nav-item dropdown custom">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">  {{Auth::user()->name}}</a>
+            <li class="nav-item dropdown custom ">
+              <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> <i class="fa-regular fa-user fs-5"></i> {{Auth::user()->name}}</a>
               
               
               
               <ul class="dropdown-menu">
                 <li>
-                  <form class="d-flex justify-content-center" method="POST" action="{{route('logout')}}">
+                  <form class="d-flex justify-content-start  " method="POST" action="{{route('logout')}}">
                     @csrf
-                    <button class="d-flex justify-content-center btn btn-danger" type="submit">Logout</button>
+                    <button class="d-flex justify-content-center btn btn-danger rounded-5" type="submit">Logout</button>
                   </form>
                 </li>
               </ul>
@@ -51,10 +54,7 @@
             
             
             
-            {{-- <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form> --}}
+
           </div>
         </div>
       </nav> </p>
