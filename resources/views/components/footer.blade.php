3e059fe0822124b59b7484d000c5f7734562b9bf
Gabriele Laiolo <gabriele.laiolo@gmail.com>
2024-01-10 15:59:37 +0100

    fix bottonoe

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,12 +5,14 @@
     <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
     <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
     {{-- <li class="nav-item"><a href="" class="nav-link px-2 text-body-secondary">Lavora con noi</a></li> --}}
+    
     <!-- Button trigger modal --> 
-  @auth
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-      Lavora con noi
-    </button> 
-  @endauth
+    @if (Auth::user()->is_revisor == 0)        
+      @auth
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Lavora con noi</button>
+      @endauth
+    @endif
+    
   </ul>
   <p class="text-center text-body-secondary">© 2023 Company, Inc</p>
 </footer>
